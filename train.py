4a1e92101b90352ddb5687c33859aec09f076b5c
Joshua Frattarola <jofratta@cisco.com>
2017-10-11 16:26:42 -0400

    adding training step

---
+++ train.py
@@ -0,0 +1,25 @@
+import numpy as np
+from data import extract_data
+from sklearn.preprocessing import StandardScaler
+from sklearn import model_selection
+from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
+from sklearn.metrics import classification_report
+from sklearn import svm
+import pickle
+
+if __name__ == '__main__':
+    X_train, y_train, X_test, y_test = extract_data()
+
+    X_scaler = StandardScaler()
+    X_scaler.fit(X_train)
+    scaled_X_train = X_scaler.transform(X_train)
+    scaled_X_test = X_scaler.transform(X_test)
+    parameters = {'kernel':('linear', 'rbf'), 'gamma':[1e-3], 'C':[1, 10]}
+    svr = svm.SVC()
+    clf = GridSearchCV(svr, parameters)
+    clf.fit(scaled_X_train, y_train)
+
+    with open('clf.pkl', 'wb') as fid:
+        pickle.dump(clf, fid)
+    with open('scaler.pkl', 'wb') as fid:
+        pickle.dump(X_scaler, fid)
