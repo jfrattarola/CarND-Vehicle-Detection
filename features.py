bbd37b8f8d3cccffebf3f23d7cddd8194fd40ef3
Joshua Frattarola <jofratta@cisco.com>
2017-10-12 13:16:38 -0400

    changed defaults

--- features.py
+++ features.py
@@ -43,13 +43,13 @@ def get_hog_features(img,
                        visualise=vis, feature_vector=feature_vec)
         return features
 
-def bin_spatial(img, size=(32, 32)):
+def bin_spatial(img, size=(8, 8)):
     color1 = cv2.resize(img[:,:,0], size).ravel()
     color2 = cv2.resize(img[:,:,1], size).ravel()
     color3 = cv2.resize(img[:,:,2], size).ravel()
     return np.hstack((color1, color2, color3))
                         
-def color_hist(img, nbins=32):    #bins_range=(0, 256)
+def color_hist(img, nbins=64):    #bins_range=(0, 256)
     # Compute the histogram of the color channels separately
     channel1_hist = np.histogram(img[:,:,0], bins=nbins)
     channel2_hist = np.histogram(img[:,:,1], bins=nbins)
