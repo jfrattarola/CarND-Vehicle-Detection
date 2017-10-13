c154dd6f5088c6ce06db252ddd2af5c5bed6f0aa
jofratta <me@joshuafrattarola.com>
2017-10-13 14:08:54 -0400

    latest

--- search_sliding.py
+++ search_sliding.py
@@ -1,5 +1,7 @@
+from utils import *
 from parameters import *
 import matplotlib.image as mpimg
+import math
 import numpy as np
 import pickle
 import cv2
@@ -34,16 +36,15 @@ def search( img, windows, clf, X_scaler, color_space=COLOR_SPACE,
 def slide(img, xview=[None, None], yview=[None, None], window_size=(64,64), overlap=(0.5, 0.5)):
     imshape = img.shape
     xview[0] = 0 if xview[0] is None else xview[0]
-    xview[1] = 0 if xview[1] is None else xview[1]
+    xview[1] = imshape[1] if xview[1] is None else xview[1]
     yview[0] = 0 if yview[0] is None else yview[0]
-    yview[1] = 0 if yview[1] is None else yview[1]
+    yview[1] = imshape[0] if yview[1] is None else yview[1]
     sizey = yview[1] - yview[0]
     sizex = xview[1] - xview[0]
     stepy = int(window_size[0] * overlap[0])
     stepx = int(window_size[1] * overlap[1])
     nysteps = int(math.floor(1.0 * sizey / stepy)) - 1
     nxsteps = int(math.floor(1.0 * sizex / stepx)) - 1
-
     windows = []
     for y in range(nysteps):
         for x in range(nxsteps):
@@ -51,29 +52,33 @@ def slide(img, xview=[None, None], yview=[None, None], window_size=(64,64), over
             ytop = yview[0] + y*stepy
             window = ((xleft, ytop), (xleft + window_size[1], ytop + window_size[0]))
             windows.append(window)
-
+    return windows
+            
 def draw_boxes( img, bboxes, color=DEFAULT_BOX_COLOR, thickness=DEFAULT_BOX_THICKNESS):
     imcopy = np.copy(img)
     for bbox in bboxes:
         cv2.rectangle(imcopy, bbox[0], bbox[1], color, thickness)
-    returne imcopy
+    return imcopy
+
+
+def get_windows(image, xviews=WINDOWS['x_limit'], yviews=WINDOWS['y_limit'], window_sizes=WINDOWS['size'], overlaps=WINDOWS['overlap']):
+    windows=[]
+    windows.append(slide(image, xview=xviews[0], yview=yviews[0], window_size=window_sizes[0], overlap=overlaps[0]))
+    windows.append(slide(image, xview=xviews[1], yview=yviews[1], window_size=window_sizes[1], overlap=overlaps[1]))
+    windows.append(slide(image, xview=xviews[2], yview=yviews[2], window_size=window_sizes[2], overlap=overlaps[2]))
+    return windows
 
 if __name__ == '__main__':
-    pass
-"""
-    # load classifier
-    print('Loading Classifier')
-    with open('clf.pkl', 'rb') as fid:
-        clf = pickle.load(fid)
-    print('Loading Scaler')
-    # load scaler
-    with open('scaler.pkl', 'rb') as fid:
-        X_scaler = pickle.load(fid)
-
-    test_images = glob.glob('test_images/*jpg')
-    for idx, image in enumerate(test_images):
-        print('Looking for cars in test image: test_images/test{}.jpg'.format(idx+1))
-        img = mpimg.imread(image)
-        out_img, _ = find_cars(img, YSTART, YSTOP, SCALES, clf, X_scaler, ORIENT, PIX_PER_CELL, CELL_PER_BLOCK, SPATIAL_SIZE, HIST_BINS, COLOR_SPACE, HOG_CHANNEL)
-        mpimg.imsave('output_images/test{}_sliding_window_test.png'.format(idx+1), out_img)
-"""
+    image = mpimg.imread('test_images/test1.jpg')
+    windows = get_windows(image)
+    images=[]
+    images.append(draw_boxes(np.copy(image), windows[0], color=(0,0,0), thickness=4))
+    images.append(draw_boxes(np.copy(image), windows[1], color=(0,0,0), thickness=4))
+    images.append(draw_boxes(np.copy(image), windows[2], color=(0,0,0), thickness=4))
+                            
+    images[0] = draw_boxes(images[0], [windows[0][5]], color=(0,255,0),thickness=8)
+    images[1] = draw_boxes(images[1], [windows[1][11]], color=(0,255,0),thickness=8)
+    images[2] = draw_boxes(images[2], [windows[2][7]], color=(0,255,0),thickness=8)
+            
+    titles=['128 x 128', '96 x 96', '80 x 80']
+    show_images_in_table(images, (3,1), fig_size=(20,14), titles=titles)
